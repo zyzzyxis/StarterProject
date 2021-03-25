13e1a797350ed18142bfa3ed884a96cdf7ddecbe
zyzzyxis <13ezimm@gmail.com>
2021-03-25 09:52:27 -0600

    protected route

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,25 @@ import About from './pages/About';
 import PRIMARY_COLOR from './styles'
 import ComponentDemo from './pages/ComponentDemo';
 import Register from './pages/Register';
+import ConnectedFetchUser from './components/FetchUser';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <>
     <h1> Hi </h1>
     <NavBar />
+    <ConnectedFetchUser>
     <Container>
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route path='/about' component={About}/>
       <Route path='/register' component={Register}/>
       <Route path='/componentDemo' component={ComponentDemo}/>
+      <ProtectedRoute exact path="/" component={Home} />
     </Switch>
     </Container>
+    </ConnectedFetchUser>
     </>
   );
 }
